b7feec83a06e7ac67ce2c6c8851529ef299450e4
Matt Keys <Matthewjkeys22@gmail.com>
2021-10-05 17:20:48 -0600

    fixed recusrion issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,23 +51,24 @@ var startGame = function() {
     startCardEl.setAttribute('style', 'display:none');
     question1El.setAttribute('style', 'display:block');
 
-    document.getElementById('btn-1a').addEventListener('click', () => {
-        toQuestion2();
-        wrongAnswer();
-    });
-    document.getElementById('btn-1b').addEventListener('click', () => {
-        toQuestion2();
-        rightAnswer();
-    });
-    document.getElementById('btn-1c').addEventListener('click', () => {
-        toQuestion2();
-        wrongAnswer();
-    });
-    document.getElementById('btn-1d').addEventListener('click', () => {
-        toQuestion2();
-        wrongAnswer();
-    });
 };
+
+document.getElementById('btn-1a').addEventListener('click', () => {
+    toQuestion2();
+    wrongAnswer();
+});
+document.getElementById('btn-1b').addEventListener('click', () => {
+    toQuestion2();
+    rightAnswer();
+});
+document.getElementById('btn-1c').addEventListener('click', () => {
+    toQuestion2();
+    wrongAnswer();
+});
+document.getElementById('btn-1d').addEventListener('click', () => {
+    toQuestion2();
+    wrongAnswer();
+});
 
 
 
@@ -75,81 +76,82 @@ var toQuestion2 = function() {
     question1El.setAttribute('style', 'display:none');
     question2El.setAttribute('style', 'display:block');
 
-    document.getElementById('btn-2a').addEventListener('click', () => {
-        toQuestion3();
-        wrongAnswer();
-    });
-    document.getElementById('btn-2b').addEventListener('click', () => {
-        toQuestion3();
-        wrongAnswer();
-    });
-    document.getElementById('btn-2c').addEventListener('click', () => {
-        toQuestion3();
-        wrongAnswer();
-    });
-    document.getElementById('btn-2d').addEventListener('click', () => {
-        toQuestion3();
-        rightAnswer();
-    });
 }
+
+document.getElementById('btn-2a').addEventListener('click', () => {
+    toQuestion3();
+    wrongAnswer();
+});
+document.getElementById('btn-2b').addEventListener('click', () => {
+    toQuestion3();
+    wrongAnswer();
+});
+document.getElementById('btn-2c').addEventListener('click', () => {
+    toQuestion3();
+    wrongAnswer();
+});
+document.getElementById('btn-2d').addEventListener('click', () => {
+    toQuestion3();
+    rightAnswer();
+});
 
 var toQuestion3 = function() {
     question2El.setAttribute('style', 'display:none');
     question3El.setAttribute('style', 'display:block');
 
-    document.getElementById('btn-3a').addEventListener('click', () => {
-        toQuestion4();
-        wrongAnswer();
-    });
-    document.getElementById('btn-3b').addEventListener('click', () => {
-        toQuestion4();
-        wrongAnswer();
-    });
-    document.getElementById('btn-3c').addEventListener('click', () => {
-        toQuestion4();
-        wrongAnswer();
-    });
-    document.getElementById('btn-3d').addEventListener('click', () => {
-        toQuestion4();
-        rightAnswer();
-    });
 }
+
+document.getElementById('btn-3a').addEventListener('click', () => {
+    toQuestion4();
+    wrongAnswer();
+});
+document.getElementById('btn-3b').addEventListener('click', () => {
+    toQuestion4();
+    wrongAnswer();
+});
+document.getElementById('btn-3c').addEventListener('click', () => {
+    toQuestion4();
+    wrongAnswer();
+});
+document.getElementById('btn-3d').addEventListener('click', () => {
+    toQuestion4();
+    rightAnswer();
+});
 
 var toQuestion4 = function() {
     question3El.setAttribute('style', 'display:none');
     question4El.setAttribute('style', 'display:block');
 
-    document.getElementById('btn-4a').addEventListener('click', () => {
-        toEndCard();
-        wrongAnswer();
-    });
-    document.getElementById('btn-4b').addEventListener('click', () => {
-        toEndCard();
-        wrongAnswer();
-    });
-    document.getElementById('btn-4c').addEventListener('click', () => {
-        toEndCard();
-        rightAnswer();
-    });
-    document.getElementById('btn-4d').addEventListener('click', () => {
-        toEndCard();
-        wrongAnswer();
-    });
 }
+
+document.getElementById('btn-4a').addEventListener('click', () => {
+    toEndCard();
+    wrongAnswer();
+});
+document.getElementById('btn-4b').addEventListener('click', () => {
+    toEndCard();
+    wrongAnswer();
+});
+document.getElementById('btn-4c').addEventListener('click', () => {
+    toEndCard();
+    rightAnswer();
+});
+document.getElementById('btn-4d').addEventListener('click', () => {
+    toEndCard();
+    wrongAnswer();
+});
 
 var toEndCard = function() {
     question4El.setAttribute('style', 'display:none');
     endCardEl.setAttribute('style', 'display:block');
 
     clearInterval(timerID);
-
-    document.getElementById('submit-score-btn').addEventListener('click', (event) => {
-        toHighscores();
-        addHighscore(event);
-    });
-
 }
 
+document.getElementById('submit-score-btn').addEventListener('click', (event) => {
+    toHighscores();
+    addHighscore(event);
+});
 
 var toHighscores = function() {
     endCardEl.setAttribute('style', 'display:none');
